16b4f09a275503c7fa03441469e2a607ab734e00
Michael Moore <michael_m@cimbura.com>
2017-10-03 23:07:28 -0500

    Set tooltip position correctly

--- assets/leafletphp.js
+++ assets/leafletphp.js
@@ -1,34 +1,45 @@
 /**
-* Tooltips don't do well on mobile / with touch devices. If we're on a touch device, 
+* Tooltip doesn't do well on mobile / with touch devices. If we're on a touch device, 
 * put the Leaflet.draw tooltips right under the active draw tool.
 */
 if ( 
 	L.Browser.touch && 
 	L.Draw !== undefined && 
-	L.Draw.Tooltips !== undefined &&
-	L.Draw.Tooltip.prototype._UpdatePosition === undefined 
+	L.Draw.Tooltip !== undefined &&
+	L.Draw.Tooltip.prototype._updatePosition === undefined 
 ) {
-	L.Draw.Tooltip.prototype._UpdatePosition = L.Draw.Tooltip.prototype.updatePosition;
+
+
+	L.Draw.Tooltip.prototype._updateContent= L.Draw.Tooltip.prototype.updateContent;
+
+	L.Draw.Tooltip.prototype.updateContent = function(labelText){
+		this._updateContent(labelText);
+		setTimeout(function(){
+			this.updatePosition();
+		}.bind(this),50);
+	};
+
+
+	L.Draw.Tooltip.prototype._updatePosition = L.Draw.Tooltip.prototype.updatePosition;
 	L.Draw.Tooltip.prototype.updatePosition = function(latlng){
-		if ( L.Browser.touch ) {
-			var active_button = document.getElementsByClassName('leaflet-draw-toolbar-button-enabled');
+		var active_button = document.getElementsByClassName('leaflet-draw-toolbar-button-enabled');
 
-			if ( active_button.length > 0 ) {
-				var a_bounds = active_button[0].getBoundingClientRect();
-				var map_bounds = this._map._container.getBoundingClientRect();
+		if ( active_button.length > 0 ) {
+			var a_bounds = active_button[0].getBoundingClientRect();
+			var map_bounds = this._map._container.getBoundingClientRect();
 
-				a_bounds.x -= map_bounds.x - a_bounds.width;
-				a_bounds.y -= map_bounds.y - a_bounds.height;
+			a_bounds.x -= map_bounds.x - a_bounds.width;
+			a_bounds.y -= map_bounds.y - a_bounds.height;
 
-				a_bounds.y += 11;
-				a_bounds.x -= 19;
-				a_bounds = this._map.containerPointToLatLng( a_bounds );	
-				this._UpdatePosition( a_bounds );
+			a_bounds.y += 11;
+			a_bounds.x -= 19;
+			a_bounds = this._map.containerPointToLatLng( a_bounds );	
 
-				latlng = a_bounds;
-			}
+			latlng = a_bounds;
 		}
 
-		this._UpdatePosition(latlng);
+		if ( latlng !== undefined ) {
+			this._updatePosition(latlng);
+		}
 	};
 }
